66ba14dcc2d29f3ed2f93c20719fdf7e484b908c
リリ <riri@ririnoMacBook-Pro.local>
2021-06-12 21:39:02 +0900

    email,password以外の値も保存できるよう記述する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :encrypted_password, :nickname, :profile ])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [ :email, :encrypted_password ])
+  end
 end
