1b0de853ad8528423aa7a1a6a787694f596b48df
リリ <riri@ririnoMacBook-Pro.local>
2021-06-16 15:32:47 +0900

    jsの記述

--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -1,4 +1,5 @@
 class HobbiesController < ApplicationController
+  before_action :set_category, only: [:new, :create]#:edit, :update, :destroy]
 
   def index
     #@hobby = Hobby.includes(:user).order("created_at DESC")
@@ -6,36 +7,49 @@ class HobbiesController < ApplicationController
 
   def new
     @hobby = Hobby.new
-    @category_parent_array = ["--"]
+    @category_parent_array = ["---"]
     Category.where(ancestry: nil).each do |parent|
       @category_parent_array << parent.name
     end
   end
 
+  def create
+    @hobby = Hobby.new(hobby_params)
+    @hobby.user_id == current_user.id
+    if @hobby.valid?
+      @hobby.save
+      Category.maltilevel_category_create(
+        @hobby,
+        params[:parent_id],
+        params[:children_id],
+        params[:grandchildren_id]
+      )
+      redirect_to root_path
+    else
+      @category_parent_array = ["---"]
+      Category.where(ancestry: nil).each do |parent|
+        @category_parent_array << parent.name
+      end
+      render :new
+    end
+  end
+
   def get_category_children
-    @category_children = Category.find(params[:parent_name]).children
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
   end
 
   def get_category_grandchildren
     @category_grandchildren = Category.find("#{params[:child_id]}").children
   end
 
-  def create
-    @hobby = Hobby.new(hobby_params)
-    unless @hobby.user_id == current_user.id
-      redirect_to root_path
-    end
-    if @hobby.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
   private
 
+  def set_category
+    @category_parent_array = Category.where(ancestry: nil)
+  end
+
   def hobby_params
-    params.require(:hobby).permit(:title, :release_date, :recommended, :synopsis).merge(user_id: current_user.id)
+    params.require(:hobby).permit(:title, :release_date, :recommended, :synopsis, :category_id).merge(user_id: current_user.id)
   end
 
 end
--- app/models/hobby.rb
+++ app/models/hobby.rb
@@ -1,4 +1,9 @@
 class Hobby < ApplicationRecord
   belongs_to :category
   belongs_to :user
+
+  with_options presence: true do
+    validates :title, :release_date, :recommended, :synopsis, :category_id, :user_id
+  end
+  
 end
