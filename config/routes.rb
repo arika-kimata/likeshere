528d82c18316c6e29c0ed8e86f684239579a251c
リリ <riri@ririnoMacBook-Pro.local>
2021-06-20 20:54:01 +0900

    ルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get 'messages/new'
   devise_for :users
   resources :users, only: :show
 
   root 'hobbies#index'
 
   resources :hobbies do
+    resources :comments, only: [:new, :create]#, :destroy]
     collection do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
