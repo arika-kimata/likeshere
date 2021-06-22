eede085303351663372e9134709cec649c62f612
リリ <riri@ririnoMacBook-Pro.local>
2021-06-23 07:07:02 +0900

    ルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   get 'messages/new'
   devise_for :users
   resources :users, only: :show
@@ -6,7 +7,8 @@ Rails.application.routes.draw do
   root 'hobbies#index'
 
   resources :hobbies do
-    resources :comments, only: [:new, :create]#, :destroy]
+    resources :messages, only: [:create]#, :destroy]
+    mount ActionCable.server, at: '/cable'
     collection do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
